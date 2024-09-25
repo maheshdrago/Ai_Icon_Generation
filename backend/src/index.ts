0951746e55be8c0122847af3eb1e3ceca5791f4e
maheshdrago <maheshdrago@gmail.com>
2024-09-24 23:43:47 -0400

    added login and signup logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
 import express, {Express} from "express";
 import rootRouter from "./routes/rootRouter";
+import { PrismaClient } from "@prisma/client";
+import { ErrorMiddleware } from "./middleware/errorMiddleware";
 
 const app:Express = express()
 const PORT: number = 3000
+export const prismaClient = new PrismaClient({
+    log:["query"]
+})
 
+app.use(express.json())
 app.use("/api", rootRouter)
+
+app.use(ErrorMiddleware)
 
 app.listen(PORT, ()=>console.log(`Started on port ${PORT}`))
