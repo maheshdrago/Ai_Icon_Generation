3d82c6a938c37f9b3a8b7e0d65585a7a32b98e52
J Lakshmi Vivek <jlakshmivivek17@gmail.com>
2024-10-03 18:24:49 -0400

    signup page added

--- backend/src/schema/auth.ts
+++ backend/src/schema/auth.ts
@@ -1,13 +1,12 @@
 import { z } from "zod";
 
-
 export const SignupSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
-    username: z.string()
-})
+  email: z.string().email(),
+  password: z.string().min(6),
+  username: z.string(),
+});
 
 export const LoginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
-})
+  email: z.string().email(),
+  password: z.string().min(6),
+});
